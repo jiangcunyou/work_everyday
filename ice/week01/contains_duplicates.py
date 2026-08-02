9489fec837180d20397b006dc1152621e56aeefe
jiangcunyou <jiangcunyou0112@gmail.com>
2026-08-02 10:30:19 -0400

    week01

--- ice/week01/contains_duplicates.py
+++ ice/week01/contains_duplicates.py
@@ -1,14 +1,14 @@
 from typing import List
 
 class Solution:
-    def containsDuplicate(self  , nums: List[int]) -> bool:
+    def containsDuplicate(self, nums: List[int]) -> bool:
         seen = set()
 
         for num in nums:
-            if num not in seen:
-                seen.add(num)
-            else:
+            if num in seen:
                 return True
+
+            seen.add(num)
 
         return False
 
