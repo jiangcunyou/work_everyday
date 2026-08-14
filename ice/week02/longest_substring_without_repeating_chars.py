5a248a333ae42e8e2c1cdb898ed5ce130f6f67e4
jiangcunyou <jiangcunyou0112@gmail.com>
2026-08-14 19:05:09 -0400

    week02

---
+++ ice/week02/longest_substring_without_repeating_chars.py
@@ -0,0 +1,24 @@
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        seen = set()
+        left = 0
+        longest = 0
+
+        for right in range(len(s)):
+            while s[right] in seen:
+                seen.remove(s[left])
+                left += 1
+
+            seen.add(s[right])
+
+            longest = max(longest, right - left + 1)
+
+        return longest
+
+
+s = "abcabcbb"
+sol = Solution()
+print(sol.lengthOfLongestSubstring(s))
+
+#TC: O(n)
+#SC: O(n)
