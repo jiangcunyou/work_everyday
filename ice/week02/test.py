75c036fc13a74fb01d788d3bb69edf7b2452e6bd
jiangcunyou <jiangcunyou0112@gmail.com>
2026-08-16 10:50:40 -0400

    week02

--- ice/week02/test.py
+++ ice/week02/test.py
@@ -1,9 +1,9 @@
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
         count = {}
-        left = 0
         max_freq = 0
         longest = 0
+        left = 0
 
         for right in range(len(s)):
             count[s[right]] = count.get(s[right], 0) + 1
@@ -14,8 +14,6 @@ class Solution:
                 left += 1
 
             longest = max(longest, right - left + 1)
-
         return longest
-
 
 print(Solution().characterReplacement("aababba", 1))
